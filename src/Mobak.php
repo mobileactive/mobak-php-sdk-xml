5a370b457e38958d6a0d51b2891e35c9b66a9231
Mike Grigorieff <grigorief@gmail.com>
2015-04-21 01:47:29 +0500

    Preview release

--- src/Mobak.php
+++ src/Mobak.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Mobak;
+
 /**
  * Class Mobak
  * @package Mobak
@@ -21,6 +22,30 @@ class Mobak
         return $this->user;
     }
 
+    /**
+     * @param array $attributes
+     *
+     * @return MobakObject
+     */
+    public function send(array $attributes)
+    {
+        $attributes['sender'] = !isset($attributes['sender']) ? 'Info' : trim($attributes['sender']);
+        $attributes['uid'] = !isset($attributes['uid']) ? sha1((new \DateTime())->format("YmdHis")) : trim($attributes['uid']);
+
+        $xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><request uid=\"{uid}\" sender=\"{sender}\">";
+        $xml .= "<message><text>{message}</text><abonent phone=\"{phone}\"></abonent></message></request>";
+
+        $xml = preg_replace("/\{uid\}/i", $attributes["uid"], $xml);
+        $xml = preg_replace("/\{sender\}/i", $attributes["sender"], $xml);
+        $xml = preg_replace("/\{message\}/i", $attributes["message"], $xml);
+        $xml = preg_replace("/\{phone\}/i", $attributes["phone"], $xml);
+
+        $request = new MobakRequest($this->getUser(), 'POST', 'messages/xml', [
+            'xml' => $xml
+        ]);
+
+        return $request->execute()->getMobakObject();
+    }
 
 
 }
